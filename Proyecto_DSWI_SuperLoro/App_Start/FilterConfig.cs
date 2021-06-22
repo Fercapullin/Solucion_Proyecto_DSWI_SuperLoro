e8881ef9d05d14bcd80011849be95bd1cd6913b3
Fercapullin <belizariof22@gmail.com>
2021-06-21 23:16:07 -0500

    Agregar archivos de proyecto.

---
+++ Proyecto_DSWI_SuperLoro/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Proyecto_DSWI_SuperLoro
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ Proyecto_DSWI_SuperLoro/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Proyecto_DSWI_SuperLoro.MvcApplication" Language="C#" %>
